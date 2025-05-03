6326e85b69f833f969acd813465b90b8e48d310e
Vishvajeet Singh Nayal <118334258+worldwin919@users.noreply.github.com>
2025-05-03 14:49:23 +0530

    Update ChewBec.java
    java solution for codeforce question chewbecca number (greedy + math + implementation)

--- Implementation/ChewBec.java
+++ Implementation/ChewBec.java
@@ -1,1 +1,20 @@
+import java.util.*;
+public class ChewBecca {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String num = sc.next();
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < num.length(); i++){
+               char cur = num.charAt(i);
+               int dig = cur - '0';
+               if(i == 0 && dig == 9) res.append("9");
+               else{
+                    int inv = 9-dig;
+                    if(inv < dig) res.append(inv);
+                    else res.append(dig);
+               }
+        }
+        System.out.println(res.toString());
+    }
+}
 
